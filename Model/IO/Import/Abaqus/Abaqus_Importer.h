6e3fb2a32ea3b103b0ea33c10ca4059d897b2076
LukGardiasz <lukasz.gardiasz@gmail.com>
2017-03-31 19:58:14 +0200

    Final version v.2 Abaqus_Import

--- Model/IO/Import/Abaqus/Abaqus_Importer.h
+++ Model/IO/Import/Abaqus/Abaqus_Importer.h
@@ -1,3 +1,4 @@
+#pragma once
 //
 // Created by Lukasz on 27.03.2017.
 //
@@ -5,7 +6,7 @@
 #ifndef MESHIS_ABAQUS_IMPORT_H
 #define MESHIS_ABAQUS_IMPORT_H
 
-#include "../MeshImporter.h"
+#include "../../../Common/CommonMeshRepresentation.h"
 
 #include <iostream>
 #include <fstream>
@@ -14,20 +15,28 @@
 
 
 
-class Abaqus_Importer : public MeshIS::Model::Import::MeshImporter{
+class Abaqus_Importer : public MeshIS::Model::Import::MeshImporter {
 
 
-    public: MeshIS::Model::Common::CMR Import(const string & absolute_file_path) override ;
+public: MeshIS::Model::Common::CMR Import(const string & absolute_file_path) override;
 
-    private: std::fstream open_abaqus_file(const string & absolute_file_path);
+private: std::fstream open_abaqus_file(const string & absolute_file_path);
 
-    private: void Set_Nodes(std::fstream data_file, MeshIS::Model::Common::CMR &Abaqus_Elements);
+private: void Set_Nodes(const string & absolute_file_path, MeshIS::Model::Common::CMR &Abaqus_Elements);
 
-    private: void Set_Elements(std::fstream data_file, MeshIS::Model::Common::CMR &Abaqus_Elements);
+private: void Set_Elements(const string & absolute_file_path, MeshIS::Model::Common::CMR &Abaqus_Elements);
 
-    private: void delete_point(string &line);
+private: void set_T4_element(string &line, MeshIS::Model::Common::CMR &Abaqus_Elements);
 
-    private: double scientific_notation(string coordinates);
+private: void set_P6_element(string &line, MeshIS::Model::Common::CMR &Abaqus_Elements);
+
+private: void delete_point(string &line);
+
+private: double scientific_notation(string &coordinates);
+
+private: bool recognize_element_T4(string line);
+
+
 
 };
 
