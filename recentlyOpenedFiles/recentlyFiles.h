fc1ca5d2b86ee3724ec30e8c659dcb04d325eb3f
Marcin <marcin.cholewka@wp.pl>
2017-03-31 21:02:40 +0200

    Final

---
+++ recentlyOpenedFiles/recentlyFiles.h
@@ -0,0 +1,35 @@
+#ifndef RecentlyFiles_H
+#define RecentlyFiles_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_helloqt.h"
+#include <Qlist>
+
+class RecentlyFiles : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	RecentlyFiles(QWidget *parent = 0);
+	~RecentlyFiles();
+
+private:
+	Ui::HelloQTClass ui;
+	const int maxRecFiles;
+	QList <QAction*> recFileActionList;
+	QString currentFilePath;
+
+	void loadFile(const QString& filePath);
+	void adjustForCurrentFile(const QString &filePath);
+	void recFilesMenu();
+	void createActionsAndConnections();
+	void updateRecentActionList();
+
+	private slots:
+		void open();
+		void openRec();
+};
+
+
+
+#endif // RecentlyFiles_H
