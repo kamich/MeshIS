d210738340288a6dbaf790d95e25f7119b93461d
LukGardiasz <lukasz.gardiasz@gmail.com>
2017-04-08 15:11:09 +0200

    confict resolve

--- Model/IO/Export/MeshExporter.h
+++ Model/IO/Export/MeshExporter.h
@@ -2,26 +2,26 @@
 #define MESHIS_MESH_EXPORTER_H_
 
 #include <string>
-#include "../../Common/CommonMeshRepresentation.h"
+#include "CommonMeshRepresentation.h"
 
 using std::string;
 
 namespace MeshIS
 {
-namespace Model
-{
-namespace Export
-{
+    namespace Model
+    {
+        namespace Export
+        {
 
-  /// Abstract class for all importing classes.
-  class MeshExporter {
-    public:
-      void   Export(const string& absolute_file_path, const CMR& mesh_data) = 0;
-  };
+            /// Abstract class for all importing classes.
+            class MeshExporter {
+            public:
+                virtual void   Export(const string& absolute_file_path, const CMR& mesh_data) = 0;
+            };
 
 
-}//! end of namespace Export
-}//! end of namespace Model
+        }//! end of namespace Export
+    }//! end of namespace Model
 }//! end of namespace MeshIS
 
 #endif /* end of include guard: MESHIS_MESH_EXPORTER_H_ */
