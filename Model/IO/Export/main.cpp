bb46f1b1b93aa81076a3fdede495d7be02e1ed0a
malgorzata.liszka <malgorzata.liszka@gmail.com>
2017-06-23 00:41:29 +0200

    small changes

--- Model/IO/Export/main.cpp
+++ Model/IO/Export/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Unv_Export.h"
+#include "CMR.h"
+
 
 CMR cmr;
 
 void loadExampleData() {//my own expample data
-	vertices = { { 0,0,0 },{ 1,1,1 },{ -1,-1,-1 },{ 1,2,1 },{ 0,1,0 },{ 2,1,1 },{ -1,0,0 },{ 1,4,1 },{ 4,3,0 },{ 5,-1,1 } };
-	cmr.elementsT4.pushback({ 0,1,2,3 });
+	cmr.vertices = { { 0,0,0 },{ 1,1,1 },{ -1,-1,-1 },{ 1,2,1 },{ 0,1,0 },{ 2,1,1 },{ -1,0,0 },{ 1,4,1 },{ 4,3,0 },{ 5,-1,1 } };
+	cmr.elementsT4.pushback({ 1,2,3,4 });
 	cmr.elementsT4.pushback({ 2,3,4,5 });
 	cmr.elementsT4.pushback({ 5,6,1,7 });
 	cmr.elementsT3.pushback({ 1,2,3 });
